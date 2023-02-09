c51353cfb17f2cb0a574f2fd5c4db6c3fe7f431f
Shanid TG <109721076+Shazard10@users.noreply.github.com>
2023-02-09 08:13:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ BUTTON_LOCK = bool(environ.get("BUTTON_LOCK", True))
 #Auto approve 
 #In private group or channel must enable request admin approval 
 CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '0').split()]
-TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour request has been approved")
+TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour request has been approved join @sk_movies12")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 # Others
